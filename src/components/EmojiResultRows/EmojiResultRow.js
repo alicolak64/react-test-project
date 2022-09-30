167131363a96c9529d0bca11876c7181e2644b2f
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-30 09:38:34 +0300

    Added testid

--- src/components/EmojiResultRows/EmojiResultRow.js
+++ src/components/EmojiResultRows/EmojiResultRow.js
@@ -14,7 +14,8 @@ export default class EmojiResultsRow extends PureComponent {
     return (
       <div
         className="component-emoji-result-row copy-to-clipboard"
-        data-clipboard-text={this.props.symbol}
+        data-clipboard-text={this.props.symbol} 
+        data-testid="row"
       >
         <img alt={this.props.title} src={src} />
         <span className="title">{this.props.title}</span>
